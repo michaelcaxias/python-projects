41d59290b2df9a02b30b8fc32cc176caffe5c533
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-30 01:57:41 -0300

    Adiciona pasta exercicios

---
+++ exercicios/ex003.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número: '))
+outroNumero = int(input('Digite outro número: '))
+soma = numero + outroNumero
+print('A soma entre', numero, 'e', outroNumero, 'é igual a:', soma)
