d4f8a6826cb7b343e7e0667080f4d48028463cf5
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-10 22:58:21 -0300

    Adicionando comentários

--- renameFiles/renameFiles.py
+++ renameFiles/renameFiles.py
@@ -2,25 +2,25 @@ import re
 import os
 import shutil
 
-mainFolder = r'/home/michael/Downloads/rename'
+mainFolder = r'/home/michael/Downloads/rename' #Pasta em que a ação será feita.
 
 def renameFile(file):
 
-  fileName, fileExtension = os.path.splitext(file)
-  fileNameNumbers = re.findall(r'\d+', fileName)[0]
+  fileName, fileExtension = os.path.splitext(file) #Puxa o nome do arquivo e a extensão do arquivo(.mp4)
+  fileNameNumbers = re.findall(r'\d+', fileName)[0] #Retorna apenas os numeros dos arquivos individualmente
 
   if fileExtension == '.mkv':
     return f'{fileNameNumbers} - BAIXAR MP4{fileExtension}'
 
-  return f'{fileNameNumbers} - HunterXHunterSeiko - RaitoKira666{fileExtension}'
+  return f'{fileNameNumbers} - HunterXHunterSeiko - RaitoKira666{fileExtension}' #Adicionar o nome dos arquivos aqui
 
 def fileLoop(root, dirs, files):
   for file in files:
-
+    originFolder = renameFile(file) 
     oldFileFullPath = os.path.join(root, file)
-    newFileFullPath = os.path.join(root, renameFile(file))
-    print(f'Renomeando arquivo {file} para {renameFile(file)}')
-    # shutil.move(oldFileFullPath, newFileFullPath)
+    newFileFullPath = os.path.join(root, originFolder)
+    print(f'Renomeando arquivo {file} para {originFolder}')
+    shutil.move(oldFileFullPath, newFileFullPath) #!!!!O shutil.move renomeará os arquivos
 
 def mainLoop():
   for root, dirs, files in os.walk(mainFolder):
